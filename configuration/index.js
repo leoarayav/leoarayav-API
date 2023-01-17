50f240229a54b176a9bc83a1dcd59e1a8cfb549a
leoarayav <leandro.araya08@inacapmail.cl>
2023-01-16 22:27:05 -0300

    feat: added mongoose nosql database.
    Ref: #9

--- configuration/index.js
+++ configuration/index.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 module.exports = {
     port: process.env.PORT || 3000,
-    database: {
+    db: {
         url: process.env.MONGO_URL || 'mongodb://leoarayav-API',
         options: {
             useNewUrlParser: true,
---
+++ database/index.js
@@ -0,0 +1,13 @@
+/**
+ * @fileoverview This file contains all database logic.
+ * @author Leo Araya
+ */
+
+const mongo = require('mongoose')
+const { db } = require('../configuration')
+
+module.exports.database = {
+    start: async (uri) => await mongo.connect(db.url || uri),
+    checkout: async () => await mongo.connection.readyState == 1,
+    disconnect: async () => await mongo.connection.close()
+}
